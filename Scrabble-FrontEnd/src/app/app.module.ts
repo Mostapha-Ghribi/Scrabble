d4cf7e2537719d5afd35d53a9973f6b3b7ab0cb7
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-05 02:42:13 +0100

    ajout d'interface inscription

--- Scrabble-FrontEnd/src/app/app.module.ts
+++ Scrabble-FrontEnd/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
