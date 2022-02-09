21b584e206ecc640cd8decbe2bbb1f8f20da0745
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-09 23:16:23 +0100

    géneration des factory(données fausses) de toutes les entités

---
+++ Scrabble-BackEnd/database/factories/JoueurFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Joueur;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JoueurFactory extends Factory
+{
+protected $model=Joueur::class;
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+}
---
+++ Scrabble-BackEnd/database/factories/PartieFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Partie;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class PartieFactory extends Factory
+{
+    protected $model=Partie::class;
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
