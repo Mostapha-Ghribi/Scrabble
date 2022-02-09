2d3227b34f62008397ba6c638f0d0e2fb957dcec
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-09 17:38:46 +0100

    fixer swagger et ajouter de deux table partie te joueur

--- Scrabble-BackEnd/app/Http/Controllers/Controller.php
+++ Scrabble-BackEnd/app/Http/Controllers/Controller.php
@@ -7,33 +7,38 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Api pour scrabble",
+ *      description="Implementation de Swagger avec Laravel9 pour documneter l'api",
+ *      @OA\Contact(
+ *          email="admin@admin.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="https://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="Demo API Server"
+ * )
+ * @OA\Tag(
+ *     name="ScrabbleGO",
+ *     description="API Endpoints de projet"
+ * )
+ * @OA\Get(
+ *     path="/projects",
+ *     @OA\Response(response="200", description="Display a listing of projects.")
+ * )
+ *
+ *
+ */
 class Controller extends BaseController
 {
-    /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title="Api pour un jeu scrabble",
-     *      description="Implementation de Swagger avec Laravel9 pour documneter l'api",
-     *      @OA\Contact(
-     *          email="admin@admin.com"
-     *      ),
-     *      @OA\License(
-     *          name="Apache 2.0",
-     *          url="https://www.apache.org/licenses/LICENSE-2.0.html"
-     *      )
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="Demo API Server"
-     * )
-     * @OA\Tag(
-     *     name="ScrabbleGO",
-     *     description="API Endpoints de projet"
-     * )
-     *
-     *
-     */
+
 
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
