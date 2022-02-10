fdb035a70e5aace1b0c2a3f196011595e0bfc80b
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-10 01:02:31 +0100

    géneration des factory(données fausses) et Seeder pour la table joueur

--- Scrabble-BackEnd/database/factories/JoueurFactory.php
+++ Scrabble-BackEnd/database/factories/JoueurFactory.php
@@ -7,11 +7,28 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class JoueurFactory extends Factory
 {
-protected $model=Joueur::class;
+    protected $model = Joueur::class;
+
     public function definition()
     {
-        return [
+      /* $chevalet = ["A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I"];
+        $chevalet2 = array_rand($chevalet, 6);
+        shuffle($chevalet2);
+        $lettres = '';
+        for ($i=0;$i<7;$chevalet2) {
+            $lettres += $chevalet2[$i];
+        }*/
 
+
+        $nom = "haithem";
+        return [
+            'nom' => $nom . rand(1, 100),
+            'photo' => 'h.png',
+            'chevalet' =>"ABCDEZC",
+            'score' => rand(1, 200),
+            'statutJoueur' => true,
+            'ordre' => 1,
+            'partie' => 1
         ];
     }
 }
