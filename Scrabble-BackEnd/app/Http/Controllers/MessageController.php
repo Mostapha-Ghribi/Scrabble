3178db2245f114cabc3856f2ba90c3967ed0b122
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-02-16 01:18:58 +0100

    documenter l'api  getallMessages ave swagger

--- Scrabble-BackEnd/app/Http/Controllers/MessageController.php
+++ Scrabble-BackEnd/app/Http/Controllers/MessageController.php
@@ -16,18 +16,106 @@ class MessageController extends Controller
     /**
      * retourner tous les message  triée par dateCreation
      */
+
+
+    /**
+     * @OA\Get(
+     *      path="/v1/messages",
+     *      operationId="index",
+     *      tags={"message"},
+     *      summary="la liste des messages",
+     *      description="la liste des messages",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     * @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *  )
+     */
+
     public function index()
     {
         return Message::latest('dateCreation')->get();
     }
 
+    /* =====================================================================================================================================
+     =====================================================================================================================================
+    */
+
+
     /**
      * retourner un  message  pard idMeddage
      */
+
+    /**
+     *
+     * @OA\Get(
+     *      path="/v1/message/{idMessage}",
+     *      operationId="getJoueur",
+     *      tags={"joueur"},
+     *      summary="Trouver un joueur a partir  de son id",
+     *
+     *  @OA\Parameter(
+     *      name="idJoueur",
+     *      in="path",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="integer"
+     *      ),
+     *   ),
+     *    @OA\Response(
+     *          response=200,
+     *          description="Opération réussie",
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     * @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     * @OA\Response(
+     *      response=404,
+     *      description="joueur inexistant"
+     *   ),
+     *  )
+     */
+
+
+
+
     public function getMessageById($idMessage)
     {
         return Message::find($idMessage)->first();
     }
+
+
+    /* =====================================================================================================================================
+   =====================================================================================================================================
+  */
 
     /**
      * retourner  Tous les messaege d'unn joueur a partir de son id
